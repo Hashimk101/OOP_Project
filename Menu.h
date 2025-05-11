ed29738572731cd472e828e0bf4269e57eaa6f9d
Hadiah-Batool <hdbatool779@gmail.com>
2025-05-11 19:25:18 +0500

    Why?

--- Menu.h
+++ Menu.h
@@ -3,6 +3,7 @@
 #include <iostream>  
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "Audio.h"
 using namespace std;
 
 
@@ -67,7 +68,7 @@ public:
             std::cout << "Failed to load MenuButton.wav" << std::endl;
         }
         OptionScrollSound.setBuffer(OptionBuffer);
-        OptionScrollSound.setVolume(60);
+        OptionScrollSound.setVolume(AudioSystem::GameVolume);
         
 
 
